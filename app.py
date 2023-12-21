4514e334ddfc8da06207b684814b852f3c3af1af
Christian <christian.remmelbe@gmail.com>
2023-12-21 13:58:14 +0100

    port changed

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def get_fibonacci():
     return jsonify({"Fibonacci Number": schreibe_zahl_rekursiv(result)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
